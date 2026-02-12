daeff62b671106b227130f32560e0c31bacc291d
Waeil Mikhaeil <102403413+Waeil55@users.noreply.github.com>
2026-02-12 14:42:57 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'merola-v5';
+const assets = ['merola.html', 'manifest.json', 'readingData.js', 'lessonData.js', 'quizGenerators.js', 'wordPartsData.js', 'storiesData.js', 'writingData.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
