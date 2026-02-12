46b45dd0bd59102f354b2a0bf080f48ebfaa2663
Waeil Mikhaeil <102403413+Waeil55@users.noreply.github.com>
2026-02-12 14:56:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-const CACHE_NAME = 'merola-v6';
-const assets = ['merola.html', 'manifest.json', 'readingData.js', 'lessonData.js', 'quizGenerators.js', 'wordPartsData.js', 'storiesData.js', 'writingData.js'];
+const CACHE_NAME = 'merola-v8';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'readingData.js',
+  'lessonData.js',
+  'quizGenerators.js',
+  'wordPartsData.js',
+  'storiesData.js',
+  'writingData.js'
+];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
